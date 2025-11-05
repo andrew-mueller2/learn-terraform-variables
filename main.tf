0655c17c2c61faa7aa91a89d782e189b7ee967b7
TEK Student <TEKstudent@vlab.lan>
2025-11-05 07:46:58 -0800

    Change environment tag from development to test

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ module "app_security_group" {
 
   tags = {
     project     = "project-alpha",
-    environment = "development"
+    environment = "test"
   }
 }
 
@@ -69,7 +69,7 @@ module "lb_security_group" {
 
   tags = {
     project     = "project-alpha",
-    environment = "development"
+    environment = "test"
   }
 }
 
@@ -110,7 +110,7 @@ module "elb_http" {
 
   tags = {
     project     = "project-alpha",
-    environment = "development"
+    environment = "test"
   }
 }
 
@@ -124,6 +124,6 @@ module "ec2_instances" {
 
   tags = {
     project     = "project-alpha",
-    environment = "development"
+    environment = "test"
   }
 }
